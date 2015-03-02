35bf7db10cda8259aa8dd695f2728dcc768aeb29
Ben Hillmann <bhillmann@outlook.com>
2015-03-01 18:25:52 -0600

    Finalized the graphs

--- cluny/cluny.py
+++ cluny/cluny.py
@@ -75,6 +75,26 @@ def make_correlation_graph(mat):
     print(len(nx.edges(G)))
     show()
 
+def plot_mat(data, name):
+    # Three subplots sharing both x/y axes
+    f, axarray = plt.subplots(8, sharex=True, sharey=True)
+    for i, row in enumerate(data):
+        axarray[i].plot(range(len(row)), row, 'g')
+    # Fine-tune figure; make subplots close to each other and hide x ticks for
+    # all but bottom plot.
+    f.subplots_adjust(hspace=0)
+    plt.setp([a.get_xticklabels() for a in f.axes], visible=False)
+    plt.setp([a.get_yticklabels() for a in f.axes], visible=False)
+    axarray[0].set_title('10 second EEG Reading for Patient')
+    axarray[int(len(axarray) / 2)].set_ylabel('Magnitude')
+    axarray[-1].set_xlabel('Time')
+    font = {'family': 'normal',
+            'weight': 'bold',
+            'size': 12}
+    plt.rc('font', **font)
+    savefig(name + "_series.png", format="png")
+    plt.show()
+
 
 class NetworkPlot:
     def __init__(self):
@@ -94,28 +114,141 @@ class NetworkPlot:
         print(len(nx.edges(G)))
         show()
 
-    def make_betweenness_centrality(self, G):
+    def make_vertex_degree_centrality(self, G, name, color="b"):
+        deg = nx.degree_centrality(G)
+        name = name + "_deg"
+        self.make_custom_plot(G, deg, name, color=color)
+
+    def make_closeness_centrality(self, G, name, color="b"):
+        close = nx.closeness_centrality(G)
+        name = name + "_close"
+        self.make_custom_plot(G, close, name, color=color)
+
+    def make_betweenness_centrality(self, G, name, color="b"):
         bet = nx.betweenness_centrality(G)
-        nodesize=np.array([bet[v] for v in G])
-        nodesize = (nodesize*1000)+1
-        self.make_custom_plot(G, nodesize)
+        name = name + "_bet"
+        self.make_custom_plot(G, bet, name, color=color)
 
-    def make_degree_centrality(self, G, name):
-        bet = nx.degree_centrality(G)
-        nodesize=np.array([bet[v] for v in G])*1000
-        self.make_custom_plot(G, nodesize, name)
+    def make_clustering_coefficient(self, G, name, color="b"):
+        cc = nx.clustering(G)
+        name = name + "_cc"
+        self.make_custom_plot(G, cc, name, color=color)
 
-    def make_custom_plot(self, G, nodesize, name):
-        nx.draw(G, self.positions, node_size=nodesize)
+    def make_custom_plot(self, G, nodesize, name, color="b"):
+        nodesize = np.array([np.sqrt(nodesize[v])+.001 for v in G])*1000
+        nx.draw_networkx_nodes(G, self.positions, node_color=color, node_size=nodesize)
         savefig(name + "_graph.png", format="png")
         show()
 
+class DifferenceNetworkPlot(NetworkPlot):
+    def __init__(self):
+        super().__init__()
+        self.degs = []
+        self.closes = []
+        self.bets = []
+        self.ccs = []
+
+
+
+    def make_vertex_degree_centrality(self, G1, G2, name):
+        deg1 = nx.degree_centrality(G1)
+        deg2 = nx.degree_centrality(G2)
+        name = name + "_deg"
+        self.make_difference(G1, name, deg1, deg2)
+
+    def make_closeness_centrality(self, G1, G2, name):
+        close1 = nx.closeness_centrality(G1)
+        close2 = nx.closeness_centrality(G2)
+        name = name + "_close"
+        self.make_difference(G1, name, close1, close2)
+
+    def make_betweenness_centrality(self, G1, G2, name):
+        bet1 = nx.betweenness_centrality(G1)
+        bet2 = nx.betweenness_centrality(G2)
+        name = name + "_bet"
+        self.make_difference(G1, name, bet1, bet2)
+
+    def make_clustering_coefficient(self, G1, G2, name):
+        cc1 = nx.clustering(G1)
+        cc2 = nx.clustering(G2)
+        name = name + "_cc"
+        self.make_difference(G1, name, cc1, cc2)
+
+    def make_difference(self, G, name, nodesize1, nodesize2):
+        nodesize1 = np.array([np.sqrt(nodesize1[v])+.001 for v in G])*1000
+        nodesize2 = np.array([np.sqrt(nodesize2[v])+.001 for v in G])*1000
+        diff = nodesize1 - nodesize2
+        name = name + "_diff"
+        pos = [i for i, d  in enumerate(diff) if d >= 0]
+        neg = [i for i, d  in enumerate(diff) if d < 0]
+        self.make_custom_plot(G, diff, pos, neg, name)
+
+    def make_custom_plot(self, G, diff, pos, neg, name):
+        nodesize = np.array([np.absolute(d) + 1 for d in diff])
+        nx.draw_networkx_nodes(G, self.positions, node_color="g", nodelist=pos, node_size=nodesize)
+        nx.draw_networkx_nodes(G, self.positions, node_color="r", nodelist=neg, node_size=nodesize)
+        savefig(name + "_graph.png", format="png")
+        show()
+
+
+
+#nx_plot = NetworkPlot()
+#for mat, name in cluny_source.generate_source():
+#    name = name.split('.')[0]
+#    plot_mat(mat[:8], name)
+    #ccm = cluny_preprocess.cross_correlation_matrix(mat)
+    #make_good_heatmap(ccm, name)
+    #ccam = cluny_preprocess.cross_correlation_adjacency_matrix(ccm)
+    #make_heatmap(ccam, name)
+    #G = nx.Graph(ccam)        
+    #nx_plot.make_degree_centrality(G, name)
+def make_ps(nx_plot, G, name, color='g'):
+    nx_plot.make_vertex_degree_centrality(G, name, color=color)
+    nx_plot.make_closeness_centrality(G, name, color=color)
+    nx_plot.make_betweenness_centrality(G, name, color=color)
+    nx_plot.make_clustering_coefficient(nx.Graph(G), name, color=color)
+
+def make_diff_ps(nx_diff_plot, G1, G2, name):
+    nx_diff_plot.make_vertex_degree_centrality(G1, G2, name)
+    nx_diff_plot.make_closeness_centrality(G1, G2, name)
+    nx_diff_plot.make_betweenness_centrality(G1, G2, name)
+    nx_diff_plot.make_clustering_coefficient(nx.Graph(G1), nx.Graph(G2), name)
+
+def degree_plot(G1, G2, name):
+    degree_sequence1 = sorted(nx.degree(G1).values(),reverse=True) # degree sequence
+    degree_sequence2 = sorted(nx.degree(G2).values(),reverse=True) # degree sequence
+    #print "Degree sequence", degree_sequence
+    plt.plot(degree_sequence1, 'g', marker='o')
+    plt.plot(degree_sequence2, 'r', marker='o')
+    plt.title("Degree rank plot")
+    plt.ylabel("degree")
+    plt.xlabel("rank")
+    plt.savefig("degree_plot" + name + ".png")
+    plt.show()
+
+
+
 nx_plot = NetworkPlot()
-for mat, name in cluny_source.generate_source():
+nx_diff_plot = DifferenceNetworkPlot()
+GM1 = nx.MultiGraph()
+GM2 = nx.MultiGraph()
+GM1.add_nodes_from([i for i in range(76)])
+GM2.add_nodes_from([i for i in range(76)])
+for i, (mat, name) in enumerate(cluny_source.generate_source()):
     name = name.split('.')[0]
     ccm = cluny_preprocess.cross_correlation_matrix(mat)
-    make_good_heatmap(ccm, name)
     ccam = cluny_preprocess.cross_correlation_adjacency_matrix(ccm)
-    make_heatmap(ccam, name)
-    G = nx.Graph(ccam)        
-    nx_plot.make_degree_centrality(G, name)
+    if i%2 == 0:
+        G1 = nx.Graph(ccam)
+        #GM1.add_edges_from(G1.edges())
+        #make_ps(nx_plot, G1, name, color='g')
+    else:
+        G2 = nx.Graph(ccam)
+        degree_plot(G1, G2, name)
+        #GM2.add_edges_from(G2.edges())
+        #make_ps(nx_plot, G2, name, color='r')
+        #make_diff_ps(nx_diff_plot, G1, G2, name)
+
+#make_ps(nx_plot, GM1, 'all_g1', color='g')
+#make_ps(nx_plot, GM2, 'all_g2', color='r')
+#make_diff_ps(nx_diff_plot, GM1, GM2, 'all_diff')
